fd2f5d6044918c4892a9f9070e9ffda79e26f00a
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-03 21:06:17 +0530

    Setting up the mongodb and routes

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ const express = require('express');
 
 const app = express();
 
+
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'))
+
+
 app.listen(3000,console.log(`Server is working fine on ${3000}`));
