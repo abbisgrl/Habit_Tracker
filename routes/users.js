08e774daaba007bde0460d74b5bc04c77a1e5fab
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-04 12:01:57 +0530

    completing the users router

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,72 @@ router.get('/login',(req,res)=>res.render('login'));
 
 router.get('/register',(req,res)=>res.render('register'));
 
-// router.get('/regis')
+router.post('/register',(req,res)=>{
+    const {name,email} = req.body;
+    let errors = [];
+    if(!name || !email){
+        errors.push({msg:'Please enter all details'});
+    }
+    if(errors.length>0){
+        res.render('register',{
+            errors,
+            name,
+            email
+        });
+    }else{
+        User.findOne({email:email}).then(user=>{
+            if(user){
+                errors.push({msg:'Email id already exist'})
+                res.render('register',{
+                    errors,
+                    name,
+                    email
+                });
+            }else{
+                const newUser = new User({
+                    name,
+                    email
+                });
+
+                newUser 
+                .save()
+                .then(user=>{
+                    req.flash(
+                        'success_msg',
+                        'You are now registered and can log in'
+                    );
+                    res.redirect('/users/login');
+                })
+                .catch(err=>console.log(err));
+            }
+            
+        });
+    }
+});
+
+router.post('/login',(req,res)=>{
+    const {name,email} = req.body;
+
+    User.findOne({
+        email:email
+    }).then(user=>{
+        if(!user){
+            let errors = [];
+            errors.push({msg:'This email is not register'});
+            res.render('login',{
+                errors,
+                name,
+                email
+            });
+        }else{
+            res.redirect(`/dashboard?user=${user.email}`);
+        }
+    });
+});
+
+router.get('/logout',(req,res)=>{
+    req.flash('success_message','You are logged out');
+    res.redirect('/users/login');
+});
+
+module.exports = router;
